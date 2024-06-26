0e16854ad988b7d1d5d09b4d78597130546afed1
charlezzhong <zheqin@umich.edu>
2024-06-26 14:11:59 -0400

    update the createUser logic, tailoring to user alreayd exist senario

--- controllers/userController.js
+++ controllers/userController.js
@@ -22,9 +22,20 @@ exports.getAllUsers = async (req, res) => {
 exports.createUser = async (req, res) => {
       try {
         console.log("new request to create user");
-        console.log(req);
+        //console.log(req);
+        const { name } = req.body;
+        if (!name) {
+            return res.status(400).json({ status: 'fail', message: 'Username is required' });
+        }
+
+        // Check if the user already exists
+        let user = await User.findOne({ name });
+        if (user) {
+            return res.status(200).json({ status: 'success', message: 'User already exists', data: { user } });
+        }
+
         const newUser = await User.create(req.body);
-        console.log("step1");
+        //console.log("step1");
     
         res.status(201).json({
             status: 'success',
@@ -33,7 +44,7 @@ exports.createUser = async (req, res) => {
             }
         });
        }catch(err){
-        console.log("step2");
+        //console.log("step2");
         res.status(400).json({
             status: 'fail',
             message: err,
